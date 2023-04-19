924f642e8f9dc7886434d8eb280090eb48324175
高木 奈央 N.T <n_takagi@saice.net>
2023-04-19 17:36:13 +0900

    some comment

--- myapp.rb
+++ myapp.rb
@@ -2,9 +2,13 @@ require 'sinatra'
 require 'sinatra/reloader'
  
 get '/' do
-    "<h1>Hello World!</h1><p>asdfghjk</p><a href='/test'>test</a>"
+    "<h1>Hello World!</h1><p>asdfghjk</p><a href='/test'>test</a><a href='/test2'>test2</a>"
 end
 
 get '/test' do
     "<h2>Hello Worldtest!</h2><p>asdfghjk</p>"
+end
+
+get '/test2' do
+    "<h2>Hello Worldtest2!</h2><p>asdfghjk</p>"
 end
