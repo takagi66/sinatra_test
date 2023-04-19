964793737cbbeb35e732836d508085c6719dd83c
高木 奈央 N.T <n_takagi@saice.net>
2023-04-20 06:28:59 +0900

    ページのtest3の追加

--- myapp.rb
+++ myapp.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
  
 get '/' do
-    "<h1>Hello World!</h1><p>asdfghjk</p><a href='/test'>test</a> <a href='/test2'>test2</a>"
+    "<h1>Hello World!</h1><p>asdfghjk</p><a href='/test'>test</a> <a href='/test2'>test2</a> <a href='/test3'>test3</a>"
 end
 
 get '/test' do
@@ -11,4 +11,8 @@ end
 
 get '/test2' do
     "<h2>Hello Worldtest2!</h2><p>asdfghjk</p>"
+end
+
+get '/test3' do
+    "<h2>Hello Worldtest3!</h2><p>asdfghjk</p>"
 end
